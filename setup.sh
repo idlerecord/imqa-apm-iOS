d481bf22b63b489812bdcdd498fe78093a0975d2
Hunta <hunta@onycom.com>
2025-03-19 10:19:21 +0900

    자동화를 위한 수정

--- setup.sh
+++ setup.sh
@@ -4,18 +4,18 @@ echo "✅Delete ~/Library/Developer/Xcode/DerivedData"
 rm -rf "~/Library/Developer/Xcode/DerivedData"
 
 echo "✅Tuist clean"
-tuist clean
-#/Users/huntapark/.local/share/mise/installs/tuist/4.41.0/bin/tuist clean
+#tuist clean
+/Users/huntapark/.local/share/mise/installs/tuist/4.41.0/bin/tuist clean
 
 echo "✅Delete files <.xcodeproj><.xcworkspace>"
 rm -rf "./*.xcodeproj" "./*.xcworkspace"
 
 echo "✅tuist generate"
-tuist generate
-#/Users/huntapark/.local/share/mise/installs/tuist/4.41.0/bin/tuist generate
+#tuist generate
+/Users/huntapark/.local/share/mise/installs/tuist/4.41.0/bin/tuist generate
 
 echo "✅pod install"
-pod install
-#/Users/huntapark/.rbenv/shims/pod install
+#pod install
+/Users/huntapark/.rbenv/shims/pod install
 
 echo "🎉setup completed"
